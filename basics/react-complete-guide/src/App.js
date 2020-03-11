2bb5404b4f07815559dbbc21b53cab212b2d578e
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-03-11 11:27:13 +0200

    fdf

--- basics/react-complete-guide/src/App.js
+++ basics/react-complete-guide/src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Person from './Person/Person';
 
-const StyledButton = styled.button`
-    background-color: ${props => props.alt ? 'red' : 'green'};
-    color: white;
-    font: inherit;
-    border: 1px solid blue;
-    padding: 8px;
-    cursor: pointer;
-    transition: all .2s;
+// const StyledButton = styled.button`
+//     background-color: ${props => props.alt ? 'red' : 'green'};
+//     color: white;
+//     font: inherit;
+//     border: 1px solid blue;
+//     padding: 8px;
+//     cursor: pointer;
+//     transition: all .2s;
 
-    &:hover {
-        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-        color: black;
-    }
-`
+//     &:hover {
+//         background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+//         color: black;
+//     }
+// `;
 
 class App extends Component {
 
@@ -107,10 +107,10 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, i am a React app</h1>
                 <p className={classes.join(' ')} >This is really working</p>
-                <StyledButton
+                <button className="button"
                     alt={this.state.showPersons}
                     onClick={this.togglePersonsHandler}>Toggle Persons
-                </StyledButton>
+                </button>
                 {persons}
             </div>
 
